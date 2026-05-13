bbe99579e08f2479b741a42dea8c5a380485388e
jwillz7667 <jwillz7667@gmail.com>
2026-05-13 09:28:10 -0500

    Initial Commit

---
+++ CareCircle/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CareCircle
+//
+//  Created by Justin Williams on 5/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
